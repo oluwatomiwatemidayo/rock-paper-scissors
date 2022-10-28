6331adf922f028409560b4084ec2042e6b712dd4
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-10-28 17:11:13 +0100

    Function playRound added with if else condition

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 // JavaScript file
+const WORD = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() {
-    let word = ['Rock', 'Paper', 'Scissors'];
-    let words = word[Math.floor(Math.random()*word.length)];
+    let words = WORD[Math.floor(Math.random()*WORD.length)];
     return words;
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() === "Rock" && computerSelection === "Paper") {
+        return "Computer wins!";
+    } else if (playerSelection.toLowerCase() === "Rock" && computerSelection === "Scissors") {
+        return "Player wins!";
+    } else if (playerSelection.toLowerCase() === "Paper" && computerSelection === "Rock") {
+        return "Player wins!";
+    } else if (playerSelection.toLowerCase() === "Paper" && computerSelection === "Scissors") {
+        return "Computer wins!";
+    } else if (playerSelection.toLowerCase() === "Scissors" && computerSelection === "Rock") {
+        return "Computer wins!";
+    } else if (playerSelection.toLowerCase() === "Scissors" && computerSelection === "Paper") {
+        return "Player wins!";
+    } else {
+        return "it's a tie!"
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
