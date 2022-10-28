5953d44092ecb7f25a9f29d4aa82cee3546fbbde
Tomiwa <72953345+oluwatomiwatemidayo@users.noreply.github.com>
2022-10-28 11:58:31 +0100

    Function getComputerChoice created and working

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // JavaScript file
 
-function getComputerChoice() {}
+function getComputerChoice() {
+    let word = ['Rock', 'Paper', 'Scissors'];
+    let words = word[Math.floor(Math.random()*word.length)];
+    return words;
+}
+
+console.log(getComputerChoice());
